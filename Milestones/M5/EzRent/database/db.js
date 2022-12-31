70defb4c8129d64413540c0910dfcf6763b808dc
Tung Nguyen <thaotung@Tungs-MacBook-Pro.local>
2022-12-30 22:24:21 -0800

    Setup cloud database

--- Milestones/M5/EzRent/database/db.js
+++ Milestones/M5/EzRent/database/db.js
@@ -3,11 +3,11 @@ function createPool() {
     const mysql = require('mysql2');
 
     const pool = mysql.createPool({
-      host: 'database-csc648-team4.cwpgbxyiuu7u.us-west-1.rds.amazonaws.com',
+      host: 'sql9.freemysqlhosting.net',
       port: 3306,
-      user: 'team4_db_admin',
-      password: 'Csc648-team4',
-      database: 'EZRentDB',
+      user: 'sql9587367',
+      password: '1w486KvXUN',
+      database: 'sql9587367',
       connectionLimit: 10,
       waitForConnections: true,
       queueLimit: 0
